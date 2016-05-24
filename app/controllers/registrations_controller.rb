26eeb2cc9af156ba6be4d68e825f502a02356d42
oschebell <oschebell@gmail.com>
2016-05-24 16:50:50 +1000

    :construction::construction: Biohazard

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+  def new
+    super
+    @skills = Skill.all
+    @locations = Location.all
+  end
+
+
+
   #def after_sign_up_path_for(resource)
   #  new_profile_path # Or :prefix_to_your_route
   #end
